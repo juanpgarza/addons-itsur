09c2907d75f20df27928c00eadbefc94a9185339
Juan Pablo Garza <juanp@juanpgarza.com>
2023-02-18 16:26:49 -0300

    [MIG] project_task_complexity_level

--- project_task_complexity_level/__manifest__.py
+++ project_task_complexity_level/__manifest__.py
@@ -1,15 +1,15 @@
-# Copyright 2020 ITSur - Juan Pablo Garza
+# Copyright 2021 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Level of complexity of project tasks",
-    "version": "12.0.1.0.0",
-    "author": "ITSur",
-    "website": "",
+    "version": "15.0.1.0.0",
+    "author": "juanpgarza",
+    "website": "https://github.com/juanpgarza/addons-itsur",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list    
-    "category": "Uncategorized",
+    "category": "Project",
     
     "license": "AGPL-3",
     "development_status": "Production/Stable",
@@ -22,5 +22,5 @@
         'views/project_task_views.xml',
         'views/project_task_complexity_views.xml',        
     ],
-    'installable': False,
+    'installable': True,
 }
