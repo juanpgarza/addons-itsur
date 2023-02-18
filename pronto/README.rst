1b2aaa9b6d36e2d5ef79211a06958fad591a13ba
Juan Pablo Garza <juanp@juanpgarza.com>
2023-02-18 13:49:41 -0300

    [MIG] pronto: funcionalidades dependientes de crm y sale_crm [CORE]

---
+++ pronto/README.rst
@@ -0,0 +1,8 @@
+=================
+Pronto
+=================
+
+Adaptaciones solicitadas por Pronto:
+
+#. (crm) Nuevo filtro para las oportunidades
+#. (sale_crm) Hacer visible la oportunidad en el pedido de ventas aunque no se esté en modo desarrollador
--- pronto/__manifest__.py
+++ pronto/__manifest__.py
@@ -11,21 +11,22 @@
     """,
 
     'author': "juanpgarza",
+    "license": "AGPL-3",
     'website': "https://github.com/juanpgarza/addons-itsur",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
 
     # any module necessary for this one to work correctly
     'depends': [
                 'base',
                 # 'account_payment_group',
-                # 'crm',
+                'crm',
                 # 'purchase',
-                # 'sale_crm',
+                'sale_crm',
                 # 'sale_order_type',
                 # 'stock',
                 # 'stock_voucher',
@@ -60,13 +61,13 @@
         # 'views/account_check_views.xml',
         # 'views/account_payment_views.xml',
         # 'views/company.xml',                
-        # 'views/crm_lead_views.xml',
+        'views/crm_lead_views.xml',
         # # 'views/menuitems.xml',
         # # 'views/purchase_order_views.xml',
         # # 'views/report_payment_group.xml',
         # 'views/report_stockpicking.xml',
         # 'views/res_partner_views.xml',
-        # 'views/sale_order_views.xml',
+        'views/sale_order_views.xml',
         # 'views/sale_tipo_cliente_views.xml',
         # 'views/stock_picking_views.xml',
         # 'views/product_pricelist_item_views.xml',
