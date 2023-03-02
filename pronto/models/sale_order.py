2e45ccf36aeea58bdc2496db4d16c884dc39302e
Juan Pablo Garza <juanp@juanpgarza.com>
2023-03-02 16:51:29 -0300

    [MIG] pronto: Calcular y mostrar el peso total de un presupuesto/pedido

--- pronto/models/sale_order.py
+++ pronto/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
@@ -6,3 +6,24 @@ class SaleOrder(models.Model):
         related='partner_id.total_due',
         string = 'Saldo'
     )
+
+    weight = fields.Float(compute='_compute_weight', string='Peso total', readonly=True, store=True)
+    weight_uom_name = fields.Char(string='Unidad de peso', compute='_compute_weight_uom_name')
+    
+    @api.depends('order_line.product_uom_qty')
+    def _compute_weight(self):
+        for order in self:            
+            # tomé como ejemplo: https://github.com/OCA/sale-reporting/blob/14.0/sale_order_weight/models/sale_order_line.py
+            lines_weight = 0.0
+            # filtro los que no son productos (secciones / notas)
+            for line in order.order_line.filtered(lambda x: not x.display_type):
+                if line.product_id:
+                    lines_weight += line.product_id.weight * line.product_uom_qty
+            order.update({
+                'weight': lines_weight,
+            })
+
+    def _compute_weight_uom_name(self):
+        weight_uom_id = self.env['product.template']._get_weight_uom_id_from_ir_config_parameter()
+        for order in self:
+            order.weight_uom_name = weight_uom_id.name
