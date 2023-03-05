2975011a5802d20392498cde7a1ae1757baa75b8
Juan Pablo Garza <juanp@juanpgarza.com>
2023-03-05 17:55:48 -0300

    [MIG] pronto: mejora a la estimación de páginas del remito (voucher) de adhoc

--- pronto/models/__init__.py
+++ pronto/models/__init__.py
@@ -25,5 +25,5 @@ from . import product_pricelist_item_history
 # from . import project_task
 from . import stock_move
 from . import stock_move_line
-# from . import stock_print_stock_voucher
+from . import stock_print_stock_voucher
 # from . import stock_quant
