1e471031e5ca7420bcc0ac80a70989dd261778d4
Juan Pablo Garza <juanp@juanpgarza.com>
2023-03-06 10:37:07 -0300

    [REM] pronto: No se usa en v12

--- pronto/models/__init__.py
+++ pronto/models/__init__.py
@@ -2,10 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-# from . import account_payment_group
-# from . import account_invoice
-# from . import account_check
-# from . import account_payment
 # from . import purchase_order
 from . import res_partner
 from . import sale_order
