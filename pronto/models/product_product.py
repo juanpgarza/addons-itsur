76a6a3203e45bd1f02a8018730c407f57c252e9e
Juan Pablo Garza <juanp@juanpgarza.com>
2023-10-27 11:51:50 -0300

    [15.0][MIG] pronto: migro funcionalidad 'Computar como stock disponible'

--- pronto/models/product_product.py
+++ pronto/models/product_product.py
@@ -4,24 +4,49 @@ from odoo.osv import expression
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    def _get_domain_locations_new(self, location_ids, company_id=False, compute_child=True):
-        domain_quant_loc, domain_move_in_loc, domain_move_out_loc = super(
-            ProductProduct, self)._get_domain_locations_new(
+    def _get_domain_locations_new(
+        self, location_ids, company_id=False, compute_child=True
+    ):
+        """
+        This is used to exclude locations if needed
+        :param location_ids:
+        :param company_id:
+        :param compute_child:
+        :return:
+        """
+        (
+            domain_quant_loc,
+            domain_move_in_loc,
+            domain_move_out_loc,
+        ) = super()._get_domain_locations_new(
             location_ids=location_ids,
             company_id=company_id,
-            compute_child=compute_child)
-        excluded_location_ids = self.env['stock.location'].search([]).filtered(lambda x: x.computar_stock_disponible==False).mapped('id')
+            compute_child=compute_child,
+        )
+        excluded_location_ids = self.env['stock.location'].search([]).filtered(lambda x: x.computar_stock_disponible==False)
         if excluded_location_ids:
-            domain_quant_loc = expression.AND([
-                [("location_id", "not in", excluded_location_ids)],
-                domain_quant_loc,
-            ])
-            domain_move_in_loc = expression.AND([
-                [("location_dest_id", "not in", excluded_location_ids)],
-                domain_move_in_loc,
-            ])
-            domain_move_out_loc = expression.AND([
-                [("location_id", "not in", excluded_location_ids)],
-                domain_move_out_loc,
-            ])
+            domain_quant_loc = expression.AND(
+                [
+                    [("location_id", "not in", excluded_location_ids.ids)],
+                    domain_quant_loc,
+                ]
+            )
+            domain_move_in_loc = expression.AND(
+                [
+                    [
+                        (
+                            "location_dest_id",
+                            "not in",
+                            excluded_location_ids.ids,
+                        )
+                    ],
+                    domain_quant_loc,
+                ]
+            )
+            domain_move_out_loc = expression.AND(
+                [
+                    [("location_id", "not in", excluded_location_ids.ids)],
+                    domain_quant_loc,
+                ]
+            )
         return domain_quant_loc, domain_move_in_loc, domain_move_out_loc
