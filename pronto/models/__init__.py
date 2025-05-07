e99406bb069067446c2cf38f1121ba581e2ffd59
Juan Pablo Garza <juanp@juanpgarza.com>
2025-05-07 19:01:36 -0300

    FIX pronto: faltaban las funcionalidades de la PO

--- pronto/models/__init__.py
+++ pronto/models/__init__.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-# from . import purchase_order
+from . import purchase_order
 from . import res_partner
 from . import sale_order
 from . import company
