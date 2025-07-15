c9591ee4057e3fe5d50d7cddcfb31678a90ff07a
Juan Pablo Garza <juanp@juanpgarza.com>
2025-07-15 20:22:56 -0300

    WIP MIG pronto

--- pronto/models/product_template.py
+++ pronto/models/product_template.py
@@ -91,37 +91,65 @@ class ProductTemplate(models.Model):
                 _logger.info("Se actualizo el costo de %d productos.", len(materias_primas) + len(productos_fabricados))
 
     @api.model
-    def create(self, val):
-        
-        # Validate 'type' and 'pack_ok' fields
-        if 'type' in val and 'pack_ok' in val:
-            if val['pack_ok'] and val['type'] != 'service':
-                raise UserError("El Tipo de producto de los packs debe ser 'Servicio'")
-        
-        # import pdb; pdb.set_trace()
-        
-        # Perform validation for saleable products
-        if not self.env.user.has_group('pronto.group_no_exigir_campos_producto_vendible'):
-            mensaje_validacion = ""
-            if val.get('sale_ok') and val.get('type') == 'consu' and val.get('is_storable'):
-                if val.get('weight', 0) == 0:
-                    mensaje_validacion += "- peso \n"
-                if val.get('volume', 0) == 0:
-                    mensaje_validacion += "- volumen \n"
-                if not val.get('image_1920'):
-                    mensaje_validacion += "- imagen del producto \n"
-                if not val.get('barcode'):
-                    mensaje_validacion += "- codigo de barras \n"
-                if not val.get('seller_ids'):
-                    mensaje_validacion += "- Proveedor \n"
+    def default_get(self, fields):
+        rec = super(ProductTemplate, self).default_get(fields)
+        rec['excluir_calculo_markup'] = False        
+        rec['route_ids'] = False        
+        rec['tracking'] = False
+        rec['taxes_id'] = False
+        rec['supplier_taxes_id'] = False
 
-            if mensaje_validacion:
-                raise ValidationError(
-                    "Debe completar los siguientes campos para que el producto pueda ser vendido: \n\n" + mensaje_validacion
-                )
+        return rec
+
+    @api.model_create_multi
+    def create(self, values):
+
+        for val in values:
+
+            # Validate 'type' and 'pack_ok' fields
+            if 'type' in val and 'pack_ok' in val:
+                if val['pack_ok'] and val['type'] != 'service':
+                    raise UserError("El Tipo de producto de los packs debe ser 'Servicio'")
+            
+            # import pdb; pdb.set_trace()
+            if not val.get('tracking'):
+                # este campo es obligatorio a nivel de base de datos
+                # hay que informarlo aunque no se marque el producto como vendible
+                if val.get('type') == 'consu':
+                    raise ValidationError("Debe informar 'Rastrear inventario' y el campo Seguimiento (Inventario/Trazabilidad)")
+                else:
+                    values['tracking'] = 'none'
+
+            # Perform validation for saleable products
+            if not self.env.user.has_group('pronto.group_no_exigir_campos_producto_vendible'):
+                mensaje_validacion = ""
+                if val.get('sale_ok') and val.get('type') == 'consu':
+                    if val.get('weight', 0) == 0:
+                        mensaje_validacion += "- peso \n"
+                    if val.get('volume', 0) == 0:
+                        mensaje_validacion += "- volumen \n"
+                    if not val.get('image_1920'):
+                        mensaje_validacion += "- imagen del producto \n"
+                    if not val.get('barcode'):
+                        mensaje_validacion += "- codigo de barras \n"
+                    if not val.get('seller_ids'):
+                        mensaje_validacion += "- Proveedor \n"
+                    if not val.get('excluir_calculo_markup'):
+                        mensaje_validacion += "- Excluir del cálculo del markup \n"
+                    if not val.get('route_ids'):
+                        mensaje_validacion += "- Rutas \n"
+                    if not val.get('taxes_id'):
+                        mensaje_validacion += "- Impuestos cliente \n"
+                    if not val.get('supplier_taxes_id'):
+                        mensaje_validacion += "- Impuestos de proveedor \n"
+
+                if mensaje_validacion:
+                    raise ValidationError(
+                        "Debe completar los siguientes campos para que el producto pueda ser vendido: \n\n" + mensaje_validacion
+                    )
 
         # Call the super method to create records
-        res = super(ProductTemplate, self).create(val)
+        res = super(ProductTemplate, self).create(values)
         return res
 
 
@@ -138,7 +166,7 @@ class ProductTemplate(models.Model):
             if not self.env.user.has_group('pronto.group_no_exigir_campos_producto_vendible'):
                 for rec in self:
                     mensaje_validacion = ""
-                    if rec.sale_ok and rec.type == 'consu' and rec.is_storable:
+                    if rec.sale_ok and rec.type == 'consu':
                         if rec.weight == 0:
                             mensaje_validacion += "- peso \n"
 
@@ -160,6 +188,13 @@ class ProductTemplate(models.Model):
                         #     else:
                         #         if item_lista_precio.compute_price == 'fixed' and item_lista_precio.fixed_price == 0:
                         #             mensaje_validacion += "- el precio (distinto de 0) en la tarifa Costo \n"
+
+                        if not rec.taxes_id:
+                            mensaje_validacion += "- Impuestos cliente \n"
+                        if not rec.supplier_taxes_id:
+                            mensaje_validacion += "- Impuestos de proveedor \n"
+                        if not rec.route_ids:
+                            mensaje_validacion += "- Rutas \n"
 
                         proveedores = rec.seller_ids
                         if not proveedores:
